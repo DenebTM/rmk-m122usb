cdbad590333d40d1c9e8dfe9d9d38bd943d61a62
DenebTM <deneb@screee.ee>
2024-11-26 02:11:39 +0100

    info-log each PS/2 data packet, handle timeouts

--- src/ps2/port.rs
+++ src/ps2/port.rs
@@ -1,5 +1,6 @@
-use defmt::{error, warn};
+use defmt::{error, info, warn};
 use embassy_rp::gpio::Input;
+use embassy_time::{with_timeout, Duration, TimeoutError};
 use pc_keyboard::{KeyCode, KeyEvent, KeyState, Ps2Decoder, ScancodeSet, ScancodeSet2};
 
 pub(crate) struct PS2Port {
@@ -43,35 +44,48 @@ impl PS2Port {
     /// wait for and decode the next PS/2 data packet
     /// if this completes a key event, add it to the event queue
     pub async fn decode_next(&mut self) {
-        let ps2_data = self.get_ps2_data().await;
-
-        let decode_result = self.ps2_decoder.add_word(ps2_data);
-        match decode_result {
-            Ok(code) => {
-                match self.scancode_processor.advance_state(code) {
-                    Ok(Some(KeyEvent { code, state })) => {
-                        self.event_queue[self.event_queue_write] = (code, state);
-                        self.event_queue_write += 1;
-                    }
-                    Ok(None) => warn!("Scan code without effect??"),
-                    Err(e) => error!("Error processing PS/2 scan code"),
-                };
+        if let Ok(ps2_data) = self.get_ps2_data().await {
+            let decode_result = self.ps2_decoder.add_word(ps2_data);
+            match decode_result {
+                Ok(code) => {
+                    match self.scancode_processor.advance_state(code) {
+                        Ok(Some(KeyEvent { code, state })) => {
+                            self.event_queue[self.event_queue_write] = (code, state);
+                            self.event_queue_write += 1;
+                        }
+                        Ok(None) => warn!("Scan code without effect??"),
+                        Err(e) => error!("Error processing PS/2 scan code"),
+                    };
+                }
+                Err(e) => {
+                    error!("Error decoding PS/2 data");
+                }
             }
-            Err(e) => {
-                error!("Error decoding PS/2 data");
-            }
-        };
+        }
     }
 
-    async fn get_ps2_data(&mut self) -> u16 {
+    async fn get_ps2_data(&mut self) -> Result<u16, TimeoutError> {
         self.clk_pin.wait_for_falling_edge().await;
+        info!("Got start of PS/2 packet");
 
         let mut data: u16 = 0;
         // read 1 start bit, 8 data bits, 1 parity bit, 1 stop bit
         for _ in 0..11 {
-            self.clk_pin.wait_for_rising_edge().await;
-            data = (data << 1) | (self.data_pin.is_high() as u16);
+            match with_timeout(
+                Duration::from_millis(1000),
+                self.clk_pin.wait_for_rising_edge(),
+            )
+            .await
+            {
+                Ok(_) => data = (data << 1) | (self.data_pin.is_high() as u16),
+
+                Err(e) => {
+                    error!("Timeout while reading PS/2 packet");
+                    return Err(e);
+                }
+            }
         }
-        return data;
+
+        Ok(data)
     }
 }
